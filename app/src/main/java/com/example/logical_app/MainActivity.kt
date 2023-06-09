f5e7455c4ad4140d6d0edc5b57a49e5ced7b74d4
Robson Cavalcanti Lins Júnior <robson.2021104910@unicap.br>
2023-06-09 00:01:22 -0300

    Just making a frot page and a first button

--- app/src/main/java/com/example/logical_app/MainActivity.kt
+++ app/src/main/java/com/example/logical_app/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.logical_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    val extra = "Oi"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun entrar(view : View){
+        val name = findViewById<EditText>(R.id.name)
+        val msg = name.text.toString()
+        val intent = Intent(this, HomeActivity::class.java).apply {
+            putExtra(extra, msg)
+        }
+        startActivity(intent)
     }
 }
