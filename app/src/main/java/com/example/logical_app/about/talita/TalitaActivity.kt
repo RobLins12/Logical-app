d44af59ddae546997b03c7f515da660c497d57e0
Talita Pacheco <80596580+Talitapach@users.noreply.github.com>
2023-06-11 15:07:37 -0300

    Add (about) - TalitaActivity

---
+++ app/src/main/java/com/example/logical_app/about/talita/TalitaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.logical_app.about.talita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.logical_app.R
+
+class TalitaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_talita)
+    }
+}
